07559c8896abe77d8fedd818968e80ee30f08d79
furugomu <furugomu@gmail.com>
2021-02-12 12:32:03 +0900

    :poop:

---
+++ pages/_app.tsx
@@ -0,0 +1,35 @@
+import App, { AppContext, AppProps } from "next/app";
+import basicAuth from "basic-auth";
+import { IncomingMessage } from "http";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+MyApp.getInitialProps = async (ctx: AppContext) => {
+  const appProps = await App.getInitialProps(ctx);
+
+  const { req, res } = ctx.ctx;
+  if (!auth(req)) {
+    res.statusCode = 401;
+    res.setHeader("WWW-Authenticate", 'Basic realm="Access to staging site"');
+    res.end();
+    return;
+  }
+  return { ...appProps };
+};
+
+const auth = (req: IncomingMessage) => {
+  // 両方未設定なら通過
+  if (!process.env.BASIC_AUTH_USER && !process.env.BASIC_AUTH_PASSWORD)
+    return true;
+  // 片方未設定はだめ
+  if (!process.env.BASIC_AUTH_USER || !process.env.BASIC_AUTH_PASSWORD)
+    return true;
+  const user = basicAuth(req);
+  if (!user) return false;
+  const { name, pass } = user;
+  if (name != process.env.BASIC_AUTH_USER) return false;
+  if (pass != process.env.BASIC_AUTH_PASSWORD) return false;
+  return true;
+};
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>こんにちは</title>
+      </Head>
+
+      <h1>こんにちは</h1>
+    </div>
+  );
+}
